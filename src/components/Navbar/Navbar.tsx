c5b623d74cd550e28c28c627cb1abe054111ae85
harshitScript <www.harshitbhawsar777@gmail.com>
2024-08-10 15:00:23 +0000

    [NAVBAR]: Navbar logo added

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,9 +1,10 @@
 import React from "react";
 import styles from './Navbar.module.css';
+import ExpenseLogo from '../../assets/svg/expense-tracker-logo.svg'
 
 const Navbar: React.FC = () => {
     return <div className={styles.navbar_outer}>
-        <span>Expense Tracker</span>
+        <img src={ExpenseLogo} alt='expense tracker logo' loading="lazy" />
     </div>
 }
 export default Navbar
