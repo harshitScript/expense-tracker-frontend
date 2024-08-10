454ebb1beae7c111d40f1069e5073305295153c6
harshitScript <www.harshitbhawsar777@gmail.com>
2024-08-10 14:24:26 +0000

    [LOGIN PAGE]: Register button added

--- src/components/LoginPage/LoginForm.tsx
+++ src/components/LoginPage/LoginForm.tsx
@@ -1,18 +1,20 @@
 import React from "react";
 import styles from "./LoginPage.module.css"
-import { Button, Card, CardContent, CardHeader, IconButton, InputAdornment, OutlinedInput, TextField, FormControl, FormHelperText } from "@mui/material";
+import { Button, Card, CardContent, IconButton, InputAdornment, OutlinedInput, TextField, FormControl, FormHelperText } from "@mui/material";
 import Visibility from '@mui/icons-material/Visibility';
 import VisibilityOff from '@mui/icons-material/VisibilityOff';
 import useToggle from "../../customHooks/useToggle";
 import { useForm } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup"
 import validationSchema from "../../validationSchema/loginFormSchema";
+import { useNavigate } from "react-router-dom";
 
 interface FormFields {
     email: string;
     password: string;
 }
 const LoginForm: React.FC = () => {
+    const navigate = useNavigate();
     const [passwordVisible, togglePasswordVisibility] = useToggle(false);
     const { register, handleSubmit, formState: { errors } } = useForm<FormFields>({
         mode: 'onBlur',
@@ -24,8 +26,10 @@ const LoginForm: React.FC = () => {
     const onSubmitError = (data: any) => {
         console.log('Submit Error Executed', data)
     }
+    const onRegisterClick = () => {
+        navigate('/sign-up');
+    }
     return <Card >
-        <CardHeader title='Login Form' />
         <CardContent style={{ padding: '1rem' }}>
             <form className={styles.login_form} onSubmit={handleSubmit(onSubmit, onSubmitError)}>
                 <TextField required autoFocus type="email" placeholder="enter email" error={!!errors.email} helperText={errors.email?.message} {...register('email')} />
@@ -45,6 +49,8 @@ const LoginForm: React.FC = () => {
                 </FormControl>
                 <Button type="submit" variant="contained" className="primary-button">Login</Button>
             </form>
+            <p style={{ textAlign: 'center' }}>OR</p>
+            <Button variant="contained" className="secondary-button" fullWidth onClick={onRegisterClick}>Register</Button>
         </CardContent>
     </Card>
 }
--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -7,10 +7,19 @@ const LoginPage: React.FC = () => {
     return <Page>
         <div className={styles.login_page_outer}>
             <section className={styles.login_section}>
+                <LoginHeader />
                 <LoginForm />
             </section>
             <section className={styles.background_section}></section>
         </div>
     </Page>
 }
+
+const LoginHeader: React.FC = () => {
+    return <div className={styles.login_header}>
+        <span>Welcome Back :)</span>
+        <span>To keep connected with us please login using your personal information by email address and password. 🧮</span>
+    </div>
+}
+
 export default LoginPage
