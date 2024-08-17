776fba50b0017dc4c0ac5f1778a50215504a5559
harshitScript <www.harshitbhawsar777@gmail.com>
2024-08-17 03:50:47 +0000

    [CATEGORY]: will only fetch categories when not fetched

--- src/pages/CategoryPage.tsx
+++ src/pages/CategoryPage.tsx
@@ -3,14 +3,15 @@ import Page from "../components/Page/Page";
 import styles from "./styles/CategoryPage.module.css";
 import Header1 from "../components/Header/Header1/Header1";
 import CategoryForm from "../components/CategoryPage/CategoryForm";
-import { useAppDispatch } from "../store/hook";
+import { useAppDispatch, useAppSelector } from "../store/hook";
 import { getCategoriesThunk } from "../store/thunk/category.thunk";
 import RenderCategories from "../components/CategoryPage/RenderCategories";
 
 const CategoryPage: React.FC = () => {
     const dispatch = useAppDispatch();
+    const { categories } = useAppSelector(state => state.category)
     // eslint-disable-next-line react-hooks/exhaustive-deps
-    useEffect(() => { dispatch(getCategoriesThunk()) }, [])
+    useEffect(() => { categories.length === 0 && dispatch(getCategoriesThunk()) }, [])
     return <Page>
         <div className={styles.category_page_outer}>
             <section className={styles.category_page_section}>
