48e687bf0e17b47900658e2fc08649257c204c29
harshitScript <www.harshitbhawsar777@gmail.com>
2024-08-17 06:25:23 +0000

    [REDUX PERSIST]: Global State persister implemented

--- src/Main.tsx
+++ src/Main.tsx
@@ -2,12 +2,15 @@ import React from "react";
 import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
-import store from "./store";
+import store, { persister } from "./store";
+import { PersistGate } from "redux-persist/integration/react";
 
 const Main: React.FC = () => {
     return <BrowserRouter>
         <Provider store={store}>
-            <App />
+            <PersistGate loading={null} persistor={persister}>
+                <App />
+            </PersistGate>
         </Provider>
     </BrowserRouter>
 }
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,15 +1,33 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import usersSlice from "./slice/users.slice";
 import authSlice from "./slice/auth.slice";
 import categorySlice from "./slice/category.slice";
+import { FLUSH, PAUSE, PERSIST, persistReducer, persistStore, PURGE, REGISTER, REHYDRATE } from 'redux-persist';
+import storageSession from "redux-persist/lib/storage/session";
 
-const store = configureStore({
-    reducer: {
-        [authSlice.name]: authSlice.reducer,
-        [usersSlice.name]: usersSlice.reducer,
-        [categorySlice.name]: categorySlice.reducer
-    }
+const persistConfig = {
+    key: 'root',
+    storage: storageSession,
+}
+
+const rootReducer = combineReducers({
+    [authSlice.name]: authSlice.reducer,
+    [usersSlice.name]: usersSlice.reducer,
+    [categorySlice.name]: categorySlice.reducer
 })
 
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+const store = configureStore({
+    reducer: persistedReducer,
+    middleware: (getDefaultMiddleware) =>
+        getDefaultMiddleware({
+            serializableCheck: {
+                ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+            },
+        }),
+})
+
+export const persister = persistStore(store);
 
 export default store;
